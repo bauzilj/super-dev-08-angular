a59a047809903de257e5d10f931a8236266e0ac0
Jean Joseph Bauzil <jeanjosephbauzil@gmail.com>
2026-04-16 13:00:01 -0300

    New update

--- src/app/imovel/imovel.ts
+++ src/app/imovel/imovel.ts
@@ -8,5 +8,25 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './imovel.css',
 })
 export class ImovelComponent {
-  
+  valor: number | null = null;
+  quantidade: number | null = null;
+  valorDoMetroQuadrado: number | null = null;
+
+  valorImovel(): void {
+
+    if (this.valor === null || this.valor <= 0) {
+      alert("Valor do imóvel deve ser preenchido e maior que 0");
+      return;
+    }
+
+    if (this.quantidade === null || this.quantidade <= 0) {
+      alert("Quantidade de metros quadrados deve ser preenchida e maior que 0");
+      return;
+    }
+
+    this.valorDoMetroQuadrado = parseFloat(
+      (this.valor / this.quantidade).toFixed(2)
+    );
+  }
+
 }
